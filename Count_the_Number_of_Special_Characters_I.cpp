846a619cf8b26e1ae53965fe7475bb7e676e22e3
jeevant010 <jeevantmudgil10@gmail.com>
2026-05-27 01:30:57 +0530

    3120 all solutino leetcode cpp

--- Count_the_Number_of_Special_Characters_I.cpp
+++ Count_the_Number_of_Special_Characters_I.cpp
@@ -4,27 +4,37 @@ using namespace std;
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-
-        vector<int> upper(26, 0);
-        vector<int> lower(26, 0);
+        bool lower[26] = {0};
+        bool upper[26] = {0};
 
         for(char ch : word) {
 
             if(ch >= 'a' && ch <= 'z')
-                lower[ch - 'a']++;
+                lower[ch - 'a'] = true;
 
             else if(ch >= 'A' && ch <= 'Z')
-                upper[ch - 'A']++;
+                upper[ch - 'A'] = true;
         }
 
         int cnt = 0;
 
         for(int i = 0; i < 26; i++) {
-            if(lower[i] > 0 && upper[i] > 0)
-                cnt++;
+            cnt += (lower[i] && upper[i]);
         }
 
         return cnt;
+
+
+        // vector<int> a(26), b(26);
+        // for(int i=0; i<word.length();i++){
+        //     if( word[i] - 'a' <= 25 && word[i] - 'a' >= 0 ) b[word[i] - 'a']++;
+        //     if( word[i] - 'A' <= 25 && word[i] - 'A' >= 0 ) a[word[i] - 'A']++;
+        // }
+        // int c = 0;
+        // for(int i=0; i<26; i++){
+        //     if( a[i] > 0 && b[i] > 0 ) c++;
+        // }
+        // return c;
     }
 };
 

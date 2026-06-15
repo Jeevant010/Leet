0e263153d20bf088d4af2c4503d8d48ed68aa011
Jeevant <jeevantmudgil10@gmail.com>
2026-06-16 03:15:09 +0530

    complete solution for 2130 leetcode cpp

--- Maximum_Twin_Sum_of_a_Linked_List.cpp
+++ Maximum_Twin_Sum_of_a_Linked_List.cpp
@@ -11,22 +11,77 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        int a = 0;
-        ListNode* l = head;
+        if( !head || !head->next ) return 0;
+        ListNode* s = head;
+        ListNode* f = head;
+        ListNode* k = NULL;
+        ListNode* t = NULL;
 
-        auto dfs = [&](this auto&& dfs, ListNode* r) -> void {
-            if (r->next)
-                dfs(r->next);
+        while( f && f->next ){
+            f = f->next->next;
+            t = s-> next;
+            s->next = k;
+            k = s;
+            s = t;
+        }
+        int ma = 0;
+        while( s && k ){
+            ma = max( ma, s->val + k->val );
+            cout<<s->val + k->val;
+            s = s->next;
+            k = k->next;
+        }
+        return ma;
 
-            a = max(a, l->val + r->val);
-            l = l->next;
-        };
 
-        dfs(head);
-        return a;
+
+
+        // stack <int> s;
+        // if( head == nullptr || head -> next == nullptr ) return 0;
+        // ListNode* sl = head;
+        // ListNode* f=head;
+        // while( f && f->next ){
+        //     s.push(sl->val);
+        //     sl = sl->next;
+        //     f = f->next->next;
+        // }
+        // int a = 0;
+        // while(sl){
+        //     a = max(a, sl->val + s.top());
+        //     sl = sl -> next;
+        //     s.pop();
+        // }
+        // return a;
+
+
+
+
+
+
+        // int a =0;
+        // ListNode* l = head;
+
+        // auto dfs = [&](this auto&& dfs, ListNode* r) -> void{
+        //     if( r->next ) dfs( r->next );
+
+        //     a = max( a, l->val + r-> val );
+        //     l = l->next;
+        // };
+        // dfs(head);
+        // return a;
     }
 };
 
